666e6c1f62be2475f78d7d8e372eac5fca64c2ce
TechDinith <80402982+TechDinith@users.noreply.github.com>
2021-03-17 00:20:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var button1= document.querySelector('.submit1');
 
 
 window.onload=function(){
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=colombo&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
+  fetch('api.openweathermap.org/data/2.5/weather?q=colombo&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
   .then(response => response.json())
   .then(data => {
     var tempValue = data['main']['temp'];
@@ -67,7 +67,7 @@ window.onload=function(){
 
 
 button.addEventListener('click', function(name){
-fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
+fetch('api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
@@ -91,7 +91,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=me
 })
 
 button1.addEventListener('click', function(name){
-  fetch('http://api.openweathermap.org/data/2.5/weather?lat='+input1.value+'&lon='+input2.value+'&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
+  fetch('api.openweathermap.org/data/2.5/weather?lat='+input1.value+'&lon='+input2.value+'&units=metric&appid=f2aac9bffa3a1ad1208bfcd8cc8f7c72')
   
   .then(response => response.json())
   .then(data => {
